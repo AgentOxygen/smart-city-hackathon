25267cdaa64cf66d5ad3bb056c7e06d3154a005b
Conor Donihoo <conordonihoo@hotmail.com>
2021-04-24 04:05:15 -0500

    adding conor-main

---
+++ conor-main.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import r2_score
+import numpy as np
+import random
+import json
+
+
+# read in json data files
+with open('elec_homes.json') as f1:
+    data_elec = json.load(f1)
+
+with open('water_homes.json') as f2:
+    data_water = json.load(f2)
+
+with open('weather_lat_lon.json') as f3:
+    data_weather = json.load(f3)
+
+
+# choose a house
+NUM_OF_HOMES = len(data_water)
+hID = random.randint(1,NUM_OF_HOMES)
+home_count = 1
+for home in data_water: # could also use data_elec, it shouldn't matter
+    if home_count == hID:
+        hID = home
+        break
+    home_count += 1
+
+
+# load the proper x-y data into data arrays
+
+
+# water consumption
+y = data_water[hID]['hourly_gal']
+
+# power consumption
+x = data_elec[hID]['hConsumed']
+
+
+# regression
+
+
+# polynomial regression as a 5th order polynomial
+model = Pipeline([('poly', PolynomialFeatures(degree=5)), ('linear', LinearRegression(fit_intercept=False))])
+model = model.fit(x[:, np.newaxis], y)
+coefficients = model.named_steps['linear'].coef_
+
+# solved function
+f = lambda v : coefficients[0] + (v)*coefficients[1] + (v**2)*coefficients[2] + (v**3)*coefficients[3] + (v**4)*coefficients[4] + (v**5)*coefficients[5] 
+
+# solved y data
+y_solved = f(x)
+
+
+# accuracy tests
+
+# coefficient of determination (1 is perfect prediction)
+print('Coefficient of Determination: %.2f' % r2_score(y,y_solved))
+
+# plotting
+plt.scatter(x,y,color='black')
+plt.plot(x,y_solved,color='blue',linewidth=3)
+plt.show()
